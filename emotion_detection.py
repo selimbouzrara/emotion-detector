863165ed147c6a36830d5741e28d3c877784f63a
selimbouzrara <selimbouzrara40@gmail.com>
2026-07-03 21:10:13 +0200

    Add emotion_detection function

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    """
+    Sends text to the Watson NLP EmotionPredict function and returns
+    the emotion scores detected in the text.
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": {"text": text_to_analyse}}
+
+    response = requests.post(url, json=myobj, headers=headers)
+    formatted_response = json.loads(response.text)
+
+    return formatted_response
